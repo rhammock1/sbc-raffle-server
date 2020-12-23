d2dbf050f862aa82f995c77234d1a52e91b0bb06
rhammock1 <rhamm1027@gmail.com>
2020-12-22 19:24:53 -0500

    Fixed warning

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const config = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/raffle'
 };
+
+module.exports = config;
